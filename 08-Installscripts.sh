404450861a83f8b9a2b99e64c5e63d225866ee48
ganeshk <gr.kamana@gmail.com>
2025-01-03 12:31:18 +0530

    Install scripts

--- 08-Installscripts.sh
+++ 08-Installscripts.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 USERID=$(id -u)
-echo "$USERID "
+#echo "$USERID "
 if [ $USERID -ne 0 ]
 then 
 echo "ERROR:: you  don't  have access to install"
 exist 1
 fi
 dnf list installed mysql
+
+if [$? -ne 0 ]
+then
+ dnf install mysql -y
+ if [$? -ne 0]
+ then echo "Installing ..........Failure"
+ exist 1
+ else
+ echo "Installing ...........Success"
