d6986e55f413ef0caec2618de2fb214589b35246
ganeshk <gr.kamana@gmail.com>
2025-01-03 11:54:31 +0530

    Conversion added

--- 07-Arrayswithloop.sh
+++ 07-Arrayswithloop.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 FRITUS=("Apple" "ORANGE" "GRAPE")
 arraylength=${#FRITUS[@]}
-for (( i=0; i<${FRITUS}; i++ ));
+for (( i=0; i<${arraylength}; i++ ));
 do
   echo "index: $i, value: ${FRITUS[$i]}"
 done
