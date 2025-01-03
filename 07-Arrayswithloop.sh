6db3b4463f251b96d2e5947ea2018dee61819c45
ganeshk <gr.kamana@gmail.com>
2025-01-03 12:04:46 +0530

    Conversion added

--- 07-Arrayswithloop.sh
+++ 07-Arrayswithloop.sh
@@ -1,10 +1,12 @@
 #!bin/bash
 FRITUS=(1 2 3 4 5 6 7 8)
 arraylength=${#FRITUS[@]}
+
 for (( i=0; i<${arraylength}; i++ ));
 do
+isEvenIfZero=$i%2;
   echo "index: $i, value: ${FRITUS[$i]}"
-  if [($i % 2) -ne 0]
+  if [isEvenIfZero -ne 0]
   then echo "$i is even number."
   else
   echo "$i is odd number."
