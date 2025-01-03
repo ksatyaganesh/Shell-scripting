ff5716300b133eaba4d1be98eecd9559dc7ebdbc
ganeshk <gr.kamana@gmail.com>
2025-01-03 11:11:24 +0530

    Conversion added

--- 05-Additiono2nums.sh
+++ 05-Additiono2nums.sh
@@ -16,14 +16,3 @@ else
     SUM=$($NUMBER1+$NUMBER2)
     echo "Sum of two numbers  is: $SUM"
 fi
-
-# NUMBER=$1
-
-# # -gt, -lt, -eq, -ge, -le
-
-# if [ $NUMBER -gt 100 ]
-# then
-#     echo "Given number is greater than 100"
-# else
-#     echo "Given number is less than or equal to 100"
-# fi
