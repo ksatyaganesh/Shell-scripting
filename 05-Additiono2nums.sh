7c5785ce0e6b3740a8efed2507da0c11e84cdb54
ganeshk <gr.kamana@gmail.com>
2025-01-03 09:31:55 +0530

    Conversion added

--- 05-Additiono2nums.sh
+++ 05-Additiono2nums.sh
@@ -4,14 +4,14 @@ NUMBER1=$1
 NUMBER2=$2
 TIMESTEMP=$(date)
 echo "Script executed at $TIMESTEMP"
-VALIDATE()
-if[$? -le 0]
-then
-echo "$NUMBER1 should not be zero"
-else
 
+if[$NUMBER1 -le 0]
+then
+echo "$NUMBER1 should not be negative"
+else if [$NUMBER2 -le 0]
+echo "$NUMBER2 should not be negative"
 SUM=$(($NUMBER1+$NUMBER2))
 echo "Sum of two numbers  is: $SUM"
 fi
 
-VALIDATE()
+
