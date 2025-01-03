bfbd782c139cf71d86f5bbdd3da7900ce53cd9af
ganeshk <gr.kamana@gmail.com>
2025-01-03 11:18:09 +0530

    Conversion added

--- 05-Additiono2nums.sh
+++ 05-Additiono2nums.sh
@@ -8,10 +8,10 @@ TIMESTEMP=$(date)
 
 if [ $NUMBER1 -lt 0 ]
 then
-    echo "NUMBER1 should not be negative"
+    echo "argument 1 should not be negative"
 elif  [ $NUMBER2 -lt 0 ]
 then
-    echo "NUMBER2 should not be negative"
+    echo "arugument2 should not be negative"
 else
     # SUM=$($NUMBER1+$NUMBER2)
     SUM=$(($NUMBER1+$NUMBER2))
