d5564ec90edb486d452ca73eb871be4706ec7e9b
ganeshk <gr.kamana@gmail.com>
2025-01-03 09:09:15 +0530

    Conversion added

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-PERSON1 = $1
-PERSON2 = $2
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1 : Hello $PERSON2"
-echo "$PERSON2 : Hi .How are are you?"
+echo "${PERSON2} : Hi .How are are you?"
 echo "$PERSON1: I am good."
