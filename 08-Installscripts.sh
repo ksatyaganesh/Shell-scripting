a73bf05cdec753b6c4d086d090c9a9cce16117b9
ganeshk <gr.kamana@gmail.com>
2025-01-04 20:34:50 +0530

    Install scripts

--- 08-Installscripts.sh
+++ 08-Installscripts.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 USERID=$(id -u)
 #echo "$USERID "
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
 if [ $USERID -ne 0 ]
 then 
 echo "ERROR:: you  don't  have access to install"
@@ -12,11 +23,11 @@ if [ $? -ne 0 ]
 then
  dnf install mysql -y
     if [ $? -ne 0]
-    then echo "Installing ..........Failure"
+    then echo "Installing ..........$R Failure $N"
     exist 1
     else
-    echo "Installing ...........Success"
+    echo "Installing ...........$G Success $N" &>>$LOG_FILE_NAME
     fi
 else
-    echo "Installed already...........Success"
+    echo "Mysql is  already $Y Installed $N"
 fi
